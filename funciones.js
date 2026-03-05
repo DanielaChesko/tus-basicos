6619fb2533ff43e6c45ae4abb1124d44304efbb0
Daniela Belén Chesko <chesko-estudios@hotmail.com>
2026-03-04 22:07:14 -0300

    agregado de boton eliminar en la seccion tareas

--- funciones.js
+++ funciones.js
@@ -112,10 +112,11 @@ botonAñadir.addEventListener('click', function(){
                 <span class="checkmark"></span>
                 <span class="texto-tarea">${texto}</span>
             </label>
+            <button class="boton-eliminar">🗑️</button>
         </li>    
     `
 
-    //agregamos a la lista la tarea
+    //agregamos a la lista la tarea a lo ultimo que se agrego
     contenedorTareas.insertAdjacentHTML('beforeend', nuevaTarea);
 
     //limpiamos input
@@ -126,5 +127,18 @@ tareaAñadir.addEventListener('keydown', function(evento){
     //para que al añadir una tarea tambien se pueda usar el ENTER (UX)
     if(evento.key === 'Enter'){
         botonAñadir.click();
+    }
+})
+
+contenedorTareas.addEventListener('click', function(evento){
+    //guardo donde se hizo el click
+    const elementoSeleccionado = evento.target;
+
+    //preguntamos si lo que se selecciono contiene la clase boton-elimninar
+    if(elementoSeleccionado.classList.contains('boton-eliminar')){
+        //guardamos la etiqueta entera (.closest sube agarrando todo hasta la etiqueta que le digamos)
+        const tareaEntera = elementoSeleccionado.closest('li');
+
+        tareaEntera.remove();
     }
 })
